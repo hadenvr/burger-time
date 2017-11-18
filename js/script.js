fc8eedec13596b7d970b019383df18697ab75c4a
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-17 19:17:21 -0500

    added read me file

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ var navList = document.querySelector("#main-nav-list");
 
 // always check to see if elements exist before writing js involving those elements
 if (navToggle && navList) {
-  navToggle.addEventListener("click", function() {
+  navToggle.addEventListener("click", function(event) {
     // This checks to see if the element has the class of close on it
+    console.log(event);
     if (navToggle.classList.contains("close")) {
       //  if the element has the close class that means the nav is open so to close it we need to remove the open class from the nav list and remove the close class from the hamburger
       navToggle.classList.remove("close")
---
+++ readme.md
@@ -0,0 +1,3 @@
+# responsive collapsable nav
+
+repo shows a responsive collapsing nav, Kurt is going to make me add more stuff :/
